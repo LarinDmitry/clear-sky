61527d433950f886b4db0e3f2d7b9400ec13599d
Dmytro Larin <d.larin@mascode.com>
2025-11-13 13:06:54 +0200

    Updates

--- assets/useTheme-BqH-_sNk.js
+++
@@ -1,1 +0,0 @@
-import{U as m,ai as t}from"./index-B-8k-ftK.js";import{a}from"./useTheme-B-oxP_yL.js";function s(){const e=a(t);return e[m]||e}export{s as u};
---
+++ assets/useTheme-C-1XZAds.js
@@ -0,0 +1,1 @@
+import{U as m,ai as t}from"./index-BE6FtOWO.js";import{a}from"./useTheme-BSbInxg-.js";function s(){const e=a(t);return e[m]||e}export{s as u};
